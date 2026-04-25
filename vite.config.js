0e6f5d9c725d32494c7887fcc50ed85e23c20532
dharanishwar <dharanishwar.2006@gmail.com>
2026-04-26 03:59:57 +0530

    svs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vike from 'vite-plugin-ssr/plugin'
 
 export default defineConfig({
-  plugins: [react(), vike()],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
